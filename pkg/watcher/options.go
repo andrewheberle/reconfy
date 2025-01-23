0bcfca0291f87f3e3802825f6146e5dbfa9c29f4
Andrew Heberle <andrewheberle@users.noreply.github.com>
2025-01-23 18:46:26 +0800

    Update options.go

--- pkg/watcher/options.go
+++ pkg/watcher/options.go
@@ -10,8 +10,27 @@ func WithFileMode(filemode os.FileMode) WatcherOption {
 	}
 }
 
+func WithWebhookUrl(webhookUrl string) WatcherOption {
+	// parse url
+	u, err := url.Parse(webhookUrl)
+	if err != nil {
+		// set as nil if parse fails
+		u = nil
+	}
+	
+	return func(w *Watcher) {
+		w.webhookUrl = u
+	}
+}
+
 func WithWebhookMethod(method string) WatcherOption {
 	return func(w *Watcher) {
 		w.webhookMethod = method
+	}
+}
+
+func WithWebhookOutput(output string) WatcherOption {
+	return func(w *Watcher) {
+		w.output = output
 	}
 }
